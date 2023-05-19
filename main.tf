6508bc7d5da46458a4ae3c116a911e37c900c519
DeepanshuDixit <134041099+DeepanshuDixit@users.noreply.github.com>
2023-05-19 12:52:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "ec2_example" {
 }
 
 resource "aws_security_group" "Deepanshu" {
-    name        = "EC2-webserver-SGDeepanshu"
+    name        = "EC2-webserver-SG12345Deepanshu"
   description = "Webserver for EC2 Instances"
 
   ingress {
